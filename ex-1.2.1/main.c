a3fe6d14c100066f40557a99b1464e2674ed6388
MatheusMorata <eumatheus09@gmail.com>
2025-08-19 18:45:08 -0300

    feat: Criando janela

--- ex-1.2.1/main.c
+++ ex-1.2.1/main.c
@@ -1,5 +1,24 @@
-#include <stdio.h>
-int main(){
+#include <SDL2/SDL.h>
+
+// Constantes
+#define Titulo "ex-1.2.1";
+#define Largura 800; 
+#define Altura 600;
+
+int main(int argc, char* args[]){
+
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *janela = SDL_CreateWindow(
+        titulo, 
+        SDL_WINDOWPOS_CENTERED, // Posição x
+        SDL_WINDOWPOS_CENTERED, // Posição y
+        Largura, 
+        Altura, 
+        SDL_WINDOW_SHOWN // Mantém visível
+    );    
+
+    SDL_Delay(30000);
     
     return 0;
 }
