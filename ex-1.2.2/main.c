9089aff00d73e4954d86768c3f03c5483dda7b5d
MatheusMorata <matlucar09@hotmail.com>
2025-08-20 18:11:02 -0300

    feat: Adicionei circulo azul

--- ex-1.2.2/main.c
+++ ex-1.2.2/main.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 
+
 int main(int argc, char* args[]){
     
     // Inicializando SDL
@@ -13,8 +14,27 @@ int main(int argc, char* args[]){
         SDL_WINDOWPOS_CENTERED, // Posição em y
         800, // Largura
         600, // Altura
-        SDL_WINDOW_SHOWN
+        SDL_WINDOW_SHOWN // Manter visível
     );
+
+    // Criando renderizador
+    SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0);
+
+    // Colorindo fundo
+    SDL_SetRenderDrawColor(renderizador, 0, 255, 64, 255);
+    SDL_RenderClear(renderizador);
+
+    // Desenhando círculo azul
+    filledCircleRGBA(
+        renderizador,
+        400, // Posição x
+        400, // Posição y
+        100, // Raio
+        0, 0, 255, 255 // RGBA
+    );
+
+
+    SDL_RenderPresent(renderizador);
 
     // TESTANDO
     SDL_Delay(1000);
