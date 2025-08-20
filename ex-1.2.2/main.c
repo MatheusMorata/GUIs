6895ab88eed4a8754e58b59a22a38dfc8804c788
MatheusMorata <matlucar09@hotmail.com>
2025-08-20 18:48:05 -0300

    feat: Adicionei losango

--- ex-1.2.2/main.c
+++ ex-1.2.2/main.c
@@ -3,7 +3,7 @@
 
 
 int main(int argc, char* args[]){
-    
+
     // Inicializando SDL
     SDL_Init(SDL_INIT_EVERYTHING);
     
@@ -24,6 +24,14 @@ int main(int argc, char* args[]){
     SDL_SetRenderDrawColor(renderizador, 0, 255, 64, 255);
     SDL_RenderClear(renderizador);
 
+    // Definindo pontos do losango
+    short pontosX[4] = {400, 700, 400, 100};  
+    short pontosY[4] = {150, 300, 450, 300};    
+
+    // Desenhando losango
+    filledPolygonRGBA(renderizador, pontosX, pontosY, 4, 255, 255, 0, 255);
+    SDL_RenderPresent(renderizador);
+
     // Desenhando círculo azul
     filledCircleRGBA(
         renderizador,
@@ -33,11 +41,8 @@ int main(int argc, char* args[]){
         0, 0, 255, 255 // RGBA
     );
 
-
-    SDL_RenderPresent(renderizador);
-
-    // TESTANDO
-    SDL_Delay(1000);
+    // EXIBINDO
+    SDL_Delay(2000);
 
     return 0;
 }
