6b3cba5eac486a54b7e4f0be5dcaf3e0615dd79c
MatheusMorata <eumatheus09@gmail.com>
2025-08-21 17:18:04 -0300

    feat: Adicionado tópicos 1 e 2

--- ex-1.4.1/main.c
+++ ex-1.4.1/main.c
@@ -3,12 +3,13 @@
 
 int main (int argc, char* args[])
 {
+
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* win = SDL_CreateWindow("Movendo um Retângulo",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
-                         1280, 720, SDL_WINDOW_SHOWN
+                         400, 400, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
@@ -17,36 +18,48 @@ int main (int argc, char* args[])
     SDL_Event event;
     bool rodando = true;
     while (rodando) {
+
+        // Posibilita que a janela seja fechada pelo alt + f4 ou pelo [X] 
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_QUIT){
                 rodando = false;
             }
-            SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-            SDL_RenderClear(ren);
-            SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-            SDL_RenderFillRect(ren, &r);
-            SDL_RenderPresent(ren);
-
-            SDL_WaitEvent(&event);
-            if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                    case SDLK_UP:
-                        r.y -= 5;
-                        break;
-                    case SDLK_DOWN:
-                        r.y += 5;
-                        break;
-                    case SDLK_LEFT:
-                        r.x -= 5;
-                        break;
-                    case SDLK_RIGHT:
-                        r.x += 5;
-                        break;
-                }
-            }
-            SDL_Delay(16);
         }
-        
+
+        // Não permite que o retângulo exceda os limites da tela
+        if(r.x > 390){
+            r.x = 390;
+        }else if(r.x < 0){
+            r.x = 0;
+        }else if(r.y > 390){
+            r.y = 390;
+        }else if(r.y < 0){
+            r.y = 0;
+        }
+
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r);
+        SDL_RenderPresent(ren);
+        SDL_WaitEvent(&event);
+        if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_UP:
+                    r.y -= 5;
+                    break;
+                case SDLK_DOWN:
+                    r.y += 5;
+                    break;
+                case SDLK_LEFT:
+                    r.x -= 5;
+                    break;
+                case SDLK_RIGHT:
+                    r.x += 5;
+                    break;
+            }
+        }
+        SDL_Delay(16);        
     }
 
     /* FINALIZACAO */
