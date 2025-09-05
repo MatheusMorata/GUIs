90b47f8fce4b804ef08b6471b501e47bb3392c45
MatheusMorata <matlucar09@hotmail.com>
2025-09-05 11:06:31 -0300

    feat: Alterações em AUX_WaitEventTimeoutCount

--- ex-1.5.3/main.c
+++ ex-1.5.3/main.c
@@ -1,9 +1,40 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
+int AUX_WaitEventTimeoutCount(SDL_Event *evt, Uint32 *ms) {
+    if (ms == NULL || *ms == 0) {
+        // Se não há tempo restante, retorna imediatamente
+        return 0;
+    }
+
+    Uint32 start = SDL_GetTicks();  // tempo inicial
+
+    // Chama a função original com o tempo restante
+    int result = SDL_WaitEventTimeout(evt, *ms);
+
+    Uint32 end = SDL_GetTicks();    // tempo final
+    Uint32 elapsed = end - start;   // tempo decorrido
+
+    // Evita underflow caso elapsed > *ms
+    if (elapsed >= *ms) {
+        *ms = 0;
+    } else {
+        *ms -= elapsed;
+    }
+
+    return result;
+}
 int main(int args, char* argc[]){
 
+    // Variáveis
+    bool rodando = true;
+    SDL_Event evento;
+    Uint32 timeout = 500;
+
+    // Inicializando SDL2
     SDL_Init(SDL_INIT_EVERYTHING);
 
+    // Criando elementos essencias (janela, renderizador e objetos)
     SDL_Window *janela = SDL_CreateWindow(
         "Exercicio 1.5.3",
         SDL_WINDOWPOS_CENTERED,
@@ -16,11 +47,27 @@ int main(int args, char* argc[]){
 
     SDL_Rect quadrado = {300, 300, 50, 50};
 
-    SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
-    SDL_RenderClear(renderizador);
-    SDL_SetRenderDrawColor(renderizador, 0, 0, 255, 0);
-    SDL_RenderFillRect(renderizador, &quadrado);
-    SDL_RenderPresent(renderizador);
-    SDL_Delay(500);
+    // Looping principal
+    while(rodando){
+
+        SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
+        SDL_RenderClear(renderizador);
+        SDL_SetRenderDrawColor(renderizador, 0, 0, 255, 0);
+        SDL_RenderFillRect(renderizador, &quadrado);
+        SDL_RenderPresent(renderizador);
+
+        if(AUX_WaitEventTimeoutCount(&evento, &timeout)){
+            if(){
+
+            }
+        }
+
+    }
+
+    // Liberando recursos
+    SDL_DestroyRenderer(renderizador);
+    SDL_DestroyWindow(janela);
+    SDL_Quit();
+
     return 0;   
 }
