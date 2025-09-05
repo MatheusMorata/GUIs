8a5f8690cf8bf603d43150f670a88ef833e8ed54
MatheusMorata <matlucar09@hotmail.com>
2025-09-05 09:45:21 -0300

    feat: Adicionei janela e renderizador

--- ex-1.5.2/main.c
+++ ex-1.5.2/main.c
@@ -1,6 +1,19 @@
 #include <SDL2\SDL.h>
 
 int main(int argc, char* args[]){
+
   SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_Janela *janela = SDL_CreateWindow(
+    "Exercicio 1.5.2",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    800, 600, 
+    SDL_WINDOW_SHOWN
+  );
+
+  SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0);
+  
+
   return 0;
 }
