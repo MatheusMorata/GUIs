5245092aac75bee1d3b54b468e469b390a0db237
MatheusMorata <matlucar09@hotmail.com>
2025-09-20 13:22:42 -0300

    feat: Redução tamanho sprite

--- ex-2.1/main.c
+++ ex-2.1/main.c
@@ -2,28 +2,27 @@
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
 
-int AUX_WaitEventTimeout(SDL_Event *evt, Uint32 *ms) {
+int AUX_WaitEventTimeout(SDL_Event *evento, Uint32 *ms) {
     if (ms == NULL) {
         return 0; // segurança
     }
 
-    Uint32 start = SDL_GetTicks(); // tempo inicial
+    Uint32 inicio = SDL_GetTicks();
+    int resultado = SDL_WaitEventTimeout(evento, *ms);
+    Uint32 fim = SDL_GetTicks();
+    Uint32 decorrido = fim - inicio;
 
-    int result = SDL_WaitEventTimeout(evt, *ms); // 1 = evento, 0 = timeout
-
-    Uint32 end = SDL_GetTicks(); // tempo final
-    Uint32 elapsed = end - start; // tempo decorrido
-
-    if (elapsed >= *ms) {
+    if (decorrido >= *ms) {
         *ms = 0;
     } else {
-        *ms -= elapsed;
+        *ms -= decorrido;
     }
 
-    return result; 
+    return resultado; 
 }
 
-int main(int agrs, char* argc[]){
+int main(int agrs, char* argc[]) {
+    int i;
 
     // Recursos essenciais
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -39,50 +38,58 @@ int main(int agrs, char* argc[]){
 
     SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0); 
 
-    SDL_Texture *anim[10];
+    SDL_Texture *animacao[10];
     char caminho[64];
-    for (int i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
+        SDL_Surface *superficie;
         snprintf(caminho, sizeof(caminho), "img/anim%d.png", i);
-        SDL_Surface *surf = IMG_Load(caminho);
-        if (!surf) { printf("Erro ao carregar %s\n", caminho); return 1; }
-        anim[i] = SDL_CreateTextureFromSurface(renderizador, surf);
-        SDL_FreeSurface(surf);
+        superficie = IMG_Load(caminho);
+        if (!superficie) { 
+            printf("Erro ao carregar %s\n", caminho); 
+            return 1; 
+        }
+        animacao[i] = SDL_CreateTextureFromSurface(renderizador, superficie);
+        SDL_FreeSurface(superficie);
     }
 
-    SDL_Surface *sMun = IMG_Load("Municao.png");
-    SDL_Surface *sEsp = IMG_Load("Espada.png");
-    SDL_Texture *texMunicao = SDL_CreateTextureFromSurface(renderizador, sMun);
-    SDL_Texture *texEspada  = SDL_CreateTextureFromSurface(renderizador, sEsp);
-    SDL_FreeSurface(sMun);
-    SDL_FreeSurface(sEsp);
-
+    SDL_Surface *superficieMunicao = IMG_Load("Municao.png");
+    SDL_Surface *superficieEspada = IMG_Load("Espada.png");
+    SDL_Texture *texturaMunicao = SDL_CreateTextureFromSurface(renderizador, superficieMunicao);
+    SDL_Texture *texturaEspada  = SDL_CreateTextureFromSurface(renderizador, superficieEspada);
+    SDL_FreeSurface(superficieMunicao);
+    SDL_FreeSurface(superficieEspada);
 
     bool rodando = true;
     SDL_Event evento;
-    int frame = 0;
+    int quadro = 0;
     int modo = 0; 
-
-
     int code = 0;
-    Uint32 timeout = 0;
+    Uint32 tempoLimite = 0;
 
+    Uint32 ultimoTempoQuadro = 0;
+    Uint32 atrasoQuadro = 100; 
 
+    SDL_Rect destino;
+    destino.x = 300;
+    destino.y = 200;
+    destino.w = 200;
+    destino.h = 200;
+
+    // LOOP PRINCIPAL
     while (rodando) {
+        Uint32 agora = SDL_GetTicks();
 
-        SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
-        SDL_RenderClear(renderizador);
-
-        if (timeout > 0) {
-            if (!AUX_WaitEventTimeout(&evento, &timeout)) {
-                SDL_Event userEvt;
-                userEvt.type = SDL_USEREVENT;
-                userEvt.user.code = code; 
-                SDL_PushEvent(&userEvt);
+        if (tempoLimite > 0) {
+            if (!AUX_WaitEventTimeout(&evento, &tempoLimite)) {
+                SDL_Event eventoUsuario;
+                eventoUsuario.type = SDL_USEREVENT;
+                eventoUsuario.user.code = code; 
+                SDL_PushEvent(&eventoUsuario);
                 code = 0;
                 continue;
             }
         } else {
-            SDL_WaitEvent(&evento);
+            SDL_PollEvent(&evento);
         }
 
         if (evento.type == SDL_QUIT) {
@@ -90,40 +97,49 @@ int main(int agrs, char* argc[]){
         }
         else if (evento.type == SDL_MOUSEBUTTONDOWN && evento.button.button == SDL_BUTTON_LEFT) {
             code++;
-            timeout = 300; 
+            tempoLimite = 300; 
         }
         else if (evento.type == SDL_USEREVENT) {
             if (evento.user.code == 2) {
                 modo = 2;
-                frame = 0;
+                quadro = 0;
+                ultimoTempoQuadro = agora;
             } else if (evento.user.code == 3) {
                 modo = 3;
-                frame = 0;
+                quadro = 0;
+                ultimoTempoQuadro = agora;
             }
         }
 
+        SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 0);
         SDL_RenderClear(renderizador);
 
         if (modo == 2 || modo == 3) {
-            if (frame < 10) {
-                SDL_RenderCopy(renderizador, anim[frame], NULL, NULL);
-                frame++;
-                SDL_Delay(100);
+            if (quadro < 10) {
+                if (agora - ultimoTempoQuadro >= atrasoQuadro) {
+                    quadro++;
+                    ultimoTempoQuadro = agora;
+                }
+                SDL_RenderCopy(renderizador, animacao[quadro < 10 ? quadro : 9], NULL, &destino);
             } else {
                 if (modo == 2)
-                    SDL_RenderCopy(renderizador, texMunicao, NULL, NULL);
+                    SDL_RenderCopy(renderizador, texturaMunicao, NULL, &destino);
                 else if (modo == 3)
-                    SDL_RenderCopy(renderizador, texEspada, NULL, NULL);
+                    SDL_RenderCopy(renderizador, texturaEspada, NULL, &destino);
             }
+        } else {
+            SDL_RenderCopy(renderizador, animacao[0], NULL, &destino);
         }
 
         SDL_RenderPresent(renderizador);
     }
 
-    // Liberando recursos
-    for (int i = 0; i < 10; i++) SDL_DestroyTexture(anim[i]);
-    SDL_DestroyTexture(texMunicao);
-    SDL_DestroyTexture(texEspada);
+    // Liberando recursos 
+    for (i = 0; i < 10; i++) {
+        SDL_DestroyTexture(animacao[i]);
+    }
+    SDL_DestroyTexture(texturaMunicao);
+    SDL_DestroyTexture(texturaEspada);
     SDL_DestroyRenderer(renderizador);
     SDL_DestroyWindow(janela);
     IMG_Quit();
