94971c400bffbc4a15d5c6359402db669ca4a38d
MatheusMorata <matlucar09@hotmail.com>
2025-09-20 11:40:18 -0300

    feat: Inicializando SDLIMG

--- ex-2.1/main.c
+++ ex-2.1/main.c
@@ -1,8 +1,12 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 int main(int agrs, char* argc[]){
 
     // Recursos essenciais
     SDL_Init(SDL_INIT_EVERYTHING);
-    
+    IMG_Init(IMG_INIT_PNG);
+
     SDL_Window *janela = SDL_CreateWindow(
         "Exercicio - 2.1",
         SDL_WINDOWPOS_CENTERED,
@@ -14,8 +18,10 @@ int main(int agrs, char* argc[]){
     SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0); 
 
 
+    // Liberando recursos
     SDL_DestroyRenderer(renderizador);
     SDL_DestroyWindow(janela);
+    IMG_Quit();
     SDL_Quit();
     return 0;
 }
