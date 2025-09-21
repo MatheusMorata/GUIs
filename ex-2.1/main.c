a3bc7c6d7b94d394c6429fb7538ecf5879eb0cc5
MatheusMorata <matlucar09@hotmail.com>
2025-09-21 14:11:44 -0300

    feat: Adicionado animação duplo clique

--- ex-2.1/main.c
+++ ex-2.1/main.c
@@ -21,20 +21,41 @@ int AUX_WaitEventTimeout(SDL_Event *evento, Uint32 *ms) {
     return resultado; 
 }
 
+void executarAnimacao(SDL_Renderer *renderer, SDL_Texture **frames, int totalFrames) {
+    for (int i = 0; i < totalFrames; i++) {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // fundo branco
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, frames[i], NULL, NULL);
+        SDL_RenderPresent(renderer); 
+    }
+}
+
 int main(int agrs, char* argc[]) {
-    // Inicialização
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG);
 
     SDL_Window *janela = SDL_CreateWindow(
-        "Exercicio - 2.1 (Detecção de Duplo Clique)",
+        "Exercicio - 2.1",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800, 600,
         SDL_WINDOW_SHOWN
     );
 
-    SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, 0); 
+    SDL_Renderer *renderizador = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Texture *frames[10];
+    char caminho[64];
+    for (int i = 0; i < 10; i++) {
+        snprintf(caminho, sizeof(caminho), "img/anim%d.png", i);
+        SDL_Surface *img = IMG_Load(caminho);
+        if (!img) {
+            SDL_Log("Erro ao carregar %s: %s", caminho, IMG_GetError());
+            return -1;
+        }
+        frames[i] = SDL_CreateTextureFromSurface(renderizador, img);
+        SDL_FreeSurface(img);
+    }
 
     SDL_Event evento;
     bool rodando = true;
@@ -42,7 +63,13 @@ int main(int agrs, char* argc[]) {
     int intervaloDuploClique = 300; 
 
     while (rodando) {
-        Uint32 timeout = 1000; 
+        Uint32 timeout = 1000;
+
+        SDL_SetRenderDrawColor(renderizador, 255, 255, 255, 255);
+        SDL_RenderClear(renderizador);
+        SDL_RenderCopy(renderizador, frames[0], NULL, NULL);
+        SDL_RenderPresent(renderizador);
+        
         if (AUX_WaitEventTimeout(&evento, &timeout)) {
             switch (evento.type) {
                 case SDL_QUIT:
@@ -54,12 +81,18 @@ int main(int agrs, char* argc[]) {
                         Uint32 agora = SDL_GetTicks();
                         if (agora - ultimoClique <= (Uint32)intervaloDuploClique) {
                             SDL_Log("Duplo clique detectado!");
+                            executarAnimacao(renderizador, frames, 10);
                         }
                         ultimoClique = agora;
                     }
                     break;
             }
         }
+    }
+
+    // Liberar recursos
+    for (int i = 0; i < 10; i++) {
+        SDL_DestroyTexture(frames[i]);
     }
 
     SDL_DestroyRenderer(renderizador);
